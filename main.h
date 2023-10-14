b6342c52fa1b0705e1dfd01a979fc36c5e0637d4
Almontheir <141272895+Almontheir@users.noreply.github.com>
2023-10-14 18:11:21 +0200

    Update main.h

--- main.h
+++ main.h
@@ -75,7 +75,8 @@ typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 
-int handle_print(const char *fmt, int *i,
+int handle_print(const char *fmt, int i, va_list list, char buffer[], int flags, int width, int precision, int size);
+
 
 va_list list, char buffer[], int flags, int width, int precision, int size)
 /****************** FUNCTIONS ******************/
