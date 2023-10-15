e53c893abf78e01570419ecf0da6fd41a45886db
Almontheir <141272895+Almontheir@users.noreply.github.com>
2023-10-15 13:30:45 +0200

    Update main.h

--- main.h
+++ main.h
@@ -73,131 +73,11 @@ struct fmt
 typedef struct fmt fmt_t;
 
 
-int _printf(const char *format, ...)
-{
-    va_list list;
-    int count = 0;
-
-    va_start(list, format);
-
-    while (*format != '\0') {
-        if (*format == '%') {
-            switch (*(++format)) {
-                case 'c':
-                    count += handle_print(format, i, list, va_arg(list, int));
-                    break;
-                case 's':
-                    count += handle_print(format, i, list, va_arg(list, char *));
-                    break;
-                case '%':
-                    count += handle_print(format, i, list, '%');
-                    break;
-                default:
-                    // Ignore unknown format specifiers.
-                    break;
-            }
-        } else {
-            count += handle_print(format, i, list, *format);
-        }
-
-        format++;
-    }
-
-    va_end(list);
-
-    return count;
-}
-
-
-va_list list, char buffer[], int flags, int width, int precision, int size)
-/****************** FUNCTIONS ******************/
-
-
-/* Funtions to print chars and strings */
-
-int print_char(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int print_string(va_list types, char buffer[],
-#ifndef MAIN_H
-
-#define MAIN_H
-
-#include <stdarg.h>
-
-#include <stdio.h>
-
-#include <unistd.h>
-
-
-#define UNUSED(x) (void)(x)
-
-#define BUFF_SIZE 1024
-
-
-/* FLAGS */
-
-#define F_MINUS 1
-
-#define F_PLUS 2
-
-#define F_ZERO 4
-
-#define F_HASH 8
-
-#define F_SPACE 16
-
-
-/* SIZES */
-
-#define S_LONG 2
-
-#define S_SHORT 1
-
-
-/**
-
- * struct fmt - Struct op
-
- *
-
- * @fmt: The format.
-
- * @fn: The function associated.
-
- */
-
-struct fmt
-
-{
-
-        char fmt;
-
-        int (*fn)(va_list, char[], int, int, int, int);
-
-};
-
-
-
-/**
-
- * typedef struct fmt fmt_t - Struct op
-
- *
-
- * @fmt: The format.
-
- * @fm_t: The function associated.
-
- */
-
-typedef struct fmt fmt_t;
-
-
 int _printf(const char *format, ...);
 
-int handle_print(const char *fmt, int i, va_list list);
+int handle_print(const char *fmt, int *i,
+
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
 /****************** FUNCTIONS ******************/
@@ -262,8 +142,6 @@ int print_non_printable(va_list types, char buffer[],
 int print_pointer(va_list types, char buffer[],
 
         int flags, int width, int precision, int size);
-
-
 /* Funciotns to handle other specifiers */
 
 int get_flags(const char *format, int *i);
@@ -329,4 +207,4 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 
-#endif
+#endif /* MAIN_H */
